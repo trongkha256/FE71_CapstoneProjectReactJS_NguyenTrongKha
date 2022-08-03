324067d3ad2a5860e58638e9de1ad28806a50095
trongkha256 <trongkha256@gmail.com>
2022-08-03 22:43:55 +0700

    fix:update new thing of headerhome

--- src/Components/HeaderHome/HeaderHome.tsx
+++ src/Components/HeaderHome/HeaderHome.tsx
@@ -23,14 +23,6 @@ const HeaderHome = (props: Props) => {
                     <li className="nav-item">
                         <NavLink className={({ isActive }) => isActive ? 'nav-link bg-warning text-light' : 'nav-link'} to={'about'}>About</NavLink>
                     </li>
-
-                    {/* <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                        <div className="dropdown-menu" aria-labelledby="dropdownId">
-                            <a className="dropdown-item" href="#">Action 1</a>
-                            <a className="dropdown-item" href="#">Action 2</a>
-                        </div>
-                    </li> */}
                 </ul>
                 <form className="d-flex my-2 my-lg-0">
                     <input className="form-control me-sm-2" type="text" placeholder="Search" />
